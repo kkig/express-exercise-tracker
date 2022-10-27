f38489a740c863012b314efa26b90b3af5351122
kkig <kigoshi.kanazawa@gmail.com>
2022-10-27 22:32:49 +0900

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+const data = JSON.stringify({
+  dataSource: 'Cluster0',
+  database: 'sample_airbnb',
+  collection: 'listingsAndReviews',
+  filter: {
+    _id: '10006546',
+  },
+});
 
+const config = {
+  method: 'post',
+  url: process.env.MONGO_DATA_API_URI + '/action/findOne',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Request-Headers': '*',
+    'api-key': process.env.MONGO_DATA_API_KEY,
+  },
+  data: data,
+};
 
-
+axios(config)
+  .then(function (res) {
+    console.log(res.data);
+  })
+  .catch(function (err) {
+    console.log('Error fetching data: ', err);
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
