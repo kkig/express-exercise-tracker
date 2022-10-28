c89249ea9ac7dfe7ce7bd2d2a65bdfd6316c78e3
kkig <kigoshi.kanazawa@gmail.com>
2022-10-29 00:24:30 +0900

    Add body-parser to retrieve data from POST

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 require('dotenv').config();
 
+/* Middlewares */
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true})); // Use to retrieve dat afrom POST
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+/* Connect to MongoDB */
 const data = JSON.stringify({
   dataSource: 'Cluster0',
   database: 'sample_airbnb', // exercise_tracker
@@ -32,11 +38,21 @@ const config = {
 
 axios(config)
   .then(function (res) {
-    console.log(res.data);
+    // console.log(res.data);
+    console.log('DB connected!');
   })
   .catch(function (err) {
     console.log('Error fetching data: ', err);
   });
+
+/* Routes */
+app.post('/api/users', (req, res) => {
+  // console.log(req.body.username);
+  const username = req.body.username;
+
+  res.json({username: username});
+  // res.json(JSON.parse(req));
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
