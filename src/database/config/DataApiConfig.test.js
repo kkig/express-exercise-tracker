8b3550394a22d3f3b5be3d811e591d537f12eeda
kkig <kigoshi.kanazawa@gmail.com>
2022-11-13 10:02:33 +0900

    Fixed test cases

--- src/database/config/DataApiConfig.test.js
+++ src/database/config/DataApiConfig.test.js
@@ -1,34 +1,13 @@
+const {config} = require('dotenv');
 const DataApiConfig = require('./DataApiConfig');
 const dataApiConfig = new DataApiConfig();
 
 require('dotenv').config();
 
-describe('DataApiConfig url.', () => {
-  it('should return object with data api url.', () => {
-    const configObj = dataApiConfig.getConfig('find', {filter: {_id: '3254'}});
-    const endpoint = '/data/v1/action/' + 'find';
-
-    expect(configObj.url).toBe(endpoint);
-  });
-
-  it('should return object for custom https endpoint.', () => {
-    const customRoute = 'myApi';
-    const filter = {_id: '3254'};
-    const configObj = dataApiConfig.getConfig(
-      {route: customRoute},
-      {filter: filter}
-    );
-    const route = '/' + customRoute;
-
-    expect(configObj.url).toBe(route);
-  });
-});
-
 describe('DataApiConfig database in req body.', () => {
   it('should return request body with datasorce.', () => {
-    const configObj = dataApiConfig.getConfig('find', {
-      filter: {_id: '3254'},
-    });
+    const option = {filter: {_id: '3254'}};
+    const configObj = new DataApiConfig().getConfig('find', option);
     const requestBody = JSON.parse(configObj.data);
 
     expect(requestBody.dataSource).toBe(process.env.MONGO_DATASOURCE);
@@ -37,36 +16,123 @@ describe('DataApiConfig database in req body.', () => {
   });
 });
 
-describe('DataApiConfig pipelines.', () => {
-  it('should return request body for a pipeline.', () => {
-    const configObj = dataApiConfig.getConfig('find', {filter: {_id: '3254'}});
-    const requestBody = JSON.parse(configObj.data);
+describe('DataApiConfig headers.', () => {
+  it('should return headers with api key.', () => {
+    const option = {filter: {_id: '3254'}};
+    const configObj = new DataApiConfig().getConfig('find', option);
 
-    expect(requestBody).toHaveProperty('filter');
-    expect(requestBody.filter).toEqual({_id: '3254'});
+    expect(configObj.headers['api-key']).toBe(process.env.MONGO_DATA_API_KEY);
   });
 
-  it('should return request body for multiple pipelines.', () => {
-    const pipelines = {
-      projection: {username: 1, _id: 1, exercise: 1},
-      limit: 5,
-    };
-    const configObj = dataApiConfig.getConfig('find', pipelines);
-    const requestBody = JSON.parse(configObj.data);
+  it('should return headers with Content-Type', () => {
+    const option = {filter: {_id: '3254'}};
+    const configObj = new DataApiConfig().getConfig('find', option);
 
-    expect(requestBody).toHaveProperty('projection');
-    expect(requestBody).toHaveProperty('limit');
+    expect(configObj.headers['Content-Type']).toBe('application/json');
+  });
+});
+
+describe('DataApiConfig + find', () => {
+  it('should be able to create config for "find" request.', () => {
+    const configObj = new DataApiConfig().getConfig('find', {
+      filter: {_id: '3254'},
+    });
+    const url = '/data/v1/action/' + 'find';
+
+    expect(configObj.url).toBe(url);
+  });
+});
+
+describe('DataApiConfig + findOne', () => {
+  it('should be able to create config with url.', () => {
+    const query = {_id: '3254'};
+    const options = {filter: query};
+
+    const configObj = new DataApiConfig().getConfig('findOne', options);
+    const url = '/data/v1/action/' + 'findOne';
+
+    expect(configObj.url).toBe(url);
   });
 
-  it('should rturn request body to add new document.', () => {
-    const doc = {username: 'John Sample', count: 0, log: []};
-    const field = {document: doc};
+  it('should be able to return config with options', () => {
+    const query = {_id: '3254'};
+    const options = {filter: query};
 
-    const configObj = dataApiConfig.getConfig('insertOne', field);
+    const configObj = new DataApiConfig().getConfig('findOne', options);
     const requestBody = JSON.parse(configObj.data);
 
-    expect(requestBody).toHaveProperty('document');
-    expect(requestBody).toHaveProperty('document.username');
-    expect(requestBody).toHaveProperty('document.count');
+    expect(requestBody.filter).toEqual(query);
+  });
+});
+
+describe('DataApiConfig + insertOne', () => {
+  it('should be able to create config with url.', () => {
+    const doc = {username: 'John Tanaka'};
+    const options = {document: doc};
+
+    const configObj = new DataApiConfig().getConfig('insertOne', options);
+    const url = '/data/v1/action/' + 'insertOne';
+
+    expect(configObj.url).toBe(url);
+  });
+
+  it('should be able to return config with options', () => {
+    const doc = {username: 'John Tanaka'};
+    const options = {document: doc};
+
+    const configObj = new DataApiConfig().getConfig('insertOne', options);
+    const requestBody = JSON.parse(configObj.data);
+
+    expect(requestBody.document).toEqual(doc);
+  });
+});
+
+describe('DataApiConfig + deleteOne', () => {
+  it('should be able to create config with url.', () => {
+    const query = {_id: 'abcd'};
+    const options = {filter: query};
+
+    const configObj = new DataApiConfig().getConfig('deleteOne', options);
+    const url = '/data/v1/action/' + 'deleteOne';
+
+    expect(configObj.url).toBe(url);
+  });
+
+  it('should be able to return config with options', () => {
+    const query = {_id: 'abcd'};
+    const options = {filter: query};
+
+    const configObj = new DataApiConfig().getConfig('insertOne', options);
+    const requestBody = JSON.parse(configObj.data);
+
+    expect(requestBody.filter).toEqual(query);
+  });
+});
+
+describe('DataApiConfig + findAndUpdate', () => {
+  it('should be able to return config with custom https url.', () => {
+    const customRoute = 'findAndUpdate';
+    const route = {route: customRoute};
+    const query = {_id: '3254'};
+    const filter = {filter: query};
+
+    const configObj = new DataApiConfig().getConfig(route, filter);
+
+    expect(configObj.url).toBe('/' + customRoute);
+  });
+
+  it('should return config with filter and update', () => {
+    const customRoute = 'findAndUpdate';
+    const route = {route: customRoute};
+    const query = {_id: '3254'};
+    const update = {$inc: {count: 1}};
+
+    const options = {filter: query, update: update};
+
+    const configObj = new DataApiConfig().getConfig(route, options);
+    const requestBody = JSON.parse(configObj.data);
+
+    expect(requestBody.filter).toEqual(query);
+    expect(requestBody.update).toEqual(update);
   });
 });
